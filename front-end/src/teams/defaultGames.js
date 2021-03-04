52f76938985eb5b4c3437fa4f612d0dbaf054a8a
Abhishek047 <abhi.shukla0047@gmail.com>
2021-03-04 14:46:58 +0530

    Added More Games

--- front-end/src/teams/defaultGames.js
+++ front-end/src/teams/defaultGames.js
@@ -1,24 +1,73 @@
 export const defaultImage =
-   "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fdefault-image.jpg?alt=media&token=e207b483-f25a-4682-8ef4-b53a6d2a754c";
+    "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fdefault-image.jpg?alt=media&token=e207b483-f25a-4682-8ef4-b53a6d2a754c";
 export const GAMES = [
-   {
-      name: "Valorant",
-      img:
-         "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fvalorant-default.png?alt=media&token=712edafc-4ef1-4165-b62b-5e1e271b05b6",
-   },
-   {
-      name: "League Of Legends",
-      img:
-         "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2FleagueOfLegends-default.jpg?alt=media&token=23ec5bb3-09a1-4b1f-b0ef-400681dd65e1",
-   },
-   {
-      name: "Overwatch",
-      img:
-         "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Foverwatch-default.jpg?alt=media&token=a03c5a61-1458-4db0-be50-7e68a0818dab",
-   },
-   {
-      name: "Rocket League",
-      img:
-         "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Frocket-default.png?alt=media&token=3fd096ff-cf57-44e0-a7f8-d5595be3f5c9",
-   },
+    {
+        name: "Valorant",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fvalorant-default.png?alt=media&token=712edafc-4ef1-4165-b62b-5e1e271b05b6",
+    },
+    {
+        name: "League Of Legends",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2FleagueOfLegends-default.jpg?alt=media&token=23ec5bb3-09a1-4b1f-b0ef-400681dd65e1",
+    },
+    {
+        name: "Overwatch",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Foverwatch-default.jpg?alt=media&token=a03c5a61-1458-4db0-be50-7e68a0818dab",
+    },
+    {
+        name: "Rocket League",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Frocket-default.png?alt=media&token=3fd096ff-cf57-44e0-a7f8-d5595be3f5c9",
+    },
+    //  {
+    //      name: "Smash Bros Ultimate",
+    //      img: "",
+    //  },
+    {
+        name: "Call of Duty: Cold War",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2FCODBO-default-min.png?alt=media&token=edffa012-32a9-45f1-8a28-4612c1eff242",
+    },
+    {
+        name: "Smite",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fsmite-default.jpg?alt=media&token=e9b026cb-c20f-4ada-81b0-0ddd4627ef3d",
+    },
+    {
+        name: "Minecraft",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fminecraft-default-min.png?alt=media&token=35cea4b4-a3c5-479c-bf84-3f3640ee7139",
+    },
+    {
+        name: "NBA 2k",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fnba-2k-default.jpg?alt=media&token=e3d4d1d6-ba37-48d0-9fd4-c9e2ef9c5d72",
+    },
+    {
+        name: "Counter Strike Global Offensive (CSGO)",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fcsgo-default-min.png?alt=media&token=f27f89e8-4149-4864-9497-64149be100dd",
+    },
+    {
+        name: "Rainbow Six Siege",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Ftcrainbowsix-default-min.png?alt=media&token=75e36f99-6a81-46b3-901c-0a8677786681",
+    },
+    {
+        name: "Apex Legends",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fapex-default.jpg?alt=media&token=fe603780-d2cd-4e90-b0fe-9fe807233b5e",
+    },
+    {
+        name: "HearthStone",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fhearthstone-default-min.png?alt=media&token=fcc4d7a6-15b7-4f91-a61f-8a99b45cfbc9",
+    },
+    {
+        name: "DOTA 2",
+        img:
+            "https://firebasestorage.googleapis.com/v0/b/spark-esport.appspot.com/o/default%2Fdota2-default-min.png?alt=media&token=96f68c73-f33e-4c5d-a3ed-7bd3f8a981f8",
+    },
 ];
