851aa801d6369101c39b5684f5cb3325b912fd17
Abhishek047 <abhi.shukla0047@gmail.com>
2021-08-21 11:32:32 +0530

    Discord Login page design

--- front-end/src/auth/SignInPage.js
+++ front-end/src/auth/SignInPage.js
@@ -1,24 +1,25 @@
-import { useState } from "react";
-import { Redirect } from "react-router-dom";
-import firebase from "firebase/app";
-import { useQueryParams } from "../routing";
-import { Alert, Box, Button, CenteredContainer, Grid, TextField, Typography } from "../ui";
-import { signIn } from "./signIn";
-import { useCurrentUserInfo } from "../users";
-import { DiscordSvgIcon } from "../img/DiscordSvgIcon";
-import { useStyles } from "./styles";
+import { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import firebase from 'firebase/app';
+import { useQueryParams } from '../routing';
+import { Alert, Box, Button, CenteredContainer, Grid, TextField, Typography } from '../ui';
+import { signIn } from './signIn';
+import { useCurrentUserInfo } from '../users';
+import { DiscordSvgIcon } from '../img/DiscordSvgIcon';
+import { useStyles } from './styles';
+import video from '../img/video0.mp4';
 
 export const SignInPage = () => {
     const { dest, email: emailFromInvitation } = useQueryParams();
-    const [email, setEmail] = useState(emailFromInvitation || "");
-    const [password, setPassword] = useState("");
-    const [networkError, setNetworkError] = useState("");
+    const [email, setEmail] = useState(emailFromInvitation || '');
+    const [password, setPassword] = useState('');
+    const [networkError, setNetworkError] = useState('');
     const { userInfo, isLoading } = useCurrentUserInfo();
-  
+
     const classes = useStyles();
 
     const onSignIn = async () => {
-        setNetworkError("");
+        setNetworkError('');
         try {
             await signIn(email, password);
         } catch (e) {
@@ -41,65 +42,87 @@ export const SignInPage = () => {
     }
 
     if (userInfo) {
-        return <Redirect to={dest || "/dashboard"} />;
+        return <Redirect to={dest || '/dashboard'} />;
     }
 
     return (
         <Grid container>
-            <Grid item xs={12} sm={6} className={classes.bgImage}>
+            <Grid item xs={12} sm={5} md={5}>
                 {/* background image */}
-            </Grid>
-            <Grid item xs={12} sm={6}>
-                <CenteredContainer>
-                    <Typography align='center'>
-                        <h1>Sign In</h1>
-                    </Typography>
-                    {networkError && (
-                        <Box mb={2}>
-                            <Alert severity='error'>{networkError}</Alert>
+                <Box className={classes.backgroundContainer}>
+                    <Box className={classes.bgImage}>
+                        {/* <video
+                            // src='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4'
+                            src={video}
+                            alt='clip'
+                            autoPlay={true}
+                            muted
+                            loop
+                        /> */}
+                    </Box>
+                </Box>
+                <Box height='100%' display='flex' alignItems='center' justifyContent='center'>
+                    <Box display='flex' flexDirection='column'>
+                        <Box className={classes.heading}>
+                            <Box className={classes.block} component='span' />
+                            <Typography variant='h2' className={classes.headingText}>
+                                SPARK
+                            </Typography>
                         </Box>
-                    )}
-                    <Box mb={2}>
-                        <TextField
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            fullWidth
-                            label='Email address'
-                            variant='outlined'
-                        />
                     </Box>
-                    <Box mb={2}>
-                        <TextField
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            fullWidth
-                            label='Password'
-                            type='password'
-                            variant='outlined'
-                        />
-                    </Box>
-                    <Box mb={2}>
-                        <Button
-                            onClick={onSignIn}
-                            fullWidth
-                            variant='contained'
-                            size='large'
-                            color='secondary'>
-                            Sign In
-                        </Button>
-                    </Box>
-                    <Box mb={2}>
-                        <Button
-                            // need to make a env variable for it
-                            href='http://localhost:8080/api/discord/login/'
-                            fullWidth
-                            className={classes.discordBtn}
-                            variant='contained'
-                            size='large'
-                            color='secondary'
-                            endIcon={<DiscordSvgIcon viewBox='0 0 71 55' />}>
-                            Sign In with Discord
-                        </Button>
+                </Box>
+            </Grid>
+            <Grid item xs={12} sm={6} md={7}>
+                <CenteredContainer>
+                    <Box className={classes.formContainer}>
+                        {networkError && (
+                            <Box mb={2}>
+                                <Alert severity='error'>{networkError}</Alert>
+                            </Box>
+                        )}
+                        <Box mb={2}>
+                            <TextField
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                fullWidth
+                                label='Email address'
+                                variant='outlined'
+                            />
+                        </Box>
+
+                        <Box mb={2}>
+                            <TextField
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                fullWidth
+                                label='Password'
+                                type='password'
+                                variant='outlined'
+                            />
+                        </Box>
+                        <Box mb={2}>
+                            <Button
+                                onClick={onSignIn}
+                                fullWidth
+                                variant='contained'
+                                size='large'
+                                color='secondary'>
+                                Sign In
+                            </Button>
+                        </Box>
+                        <Box mb={2}>
+                            <Button
+                                // need to make a env variable for it
+                                href='http://localhost:8080/api/discord/login/'
+                                fullWidth
+                                className={classes.discordBtn}
+                                variant='contained'
+                                size='large'
+                                color='secondary'
+                                endIcon={<DiscordSvgIcon viewBox='0 0 71 55' />}>
+                                Sign In with Discord
+                            </Button>
+                        </Box>
                     </Box>
                 </CenteredContainer>
             </Grid>
--- front-end/src/auth/styles.js
+++ front-end/src/auth/styles.js
@@ -1,24 +1,99 @@
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from '@material-ui/styles';
+import bg from '../img/signIn.jpg';
 
-export const useStyles = makeStyles((theme) => ({
+export const useStyles = makeStyles(theme => ({
     // global signup page style
+    backgroundContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+        zIndex: '-10',
+    },
     bgImage: {
-        backgroundColor: "#333",
-        height: "90vh",
+        height: '100%',
+        position: 'relative',
+        background: `url(${bg})`,
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        zIndex: -1,
+        '&::before': {
+            position: 'absolute',
+            zIndex: 0,
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            content: "''",
+            backgroundImage: 'linear-gradient(90deg, rgba(4,4,4,0.4) 0%, rgba(7,7,7,0.6) 100%)',
+        },
+    },
+    // sign in page heading
+    heading: {
+        position: 'relative',
+        padding: '5px 10px',
+    },
+    block: {
+        position: 'absolute',
+        zIndex: 1,
+        width: '100%',
+        height: '100%',
+        top: 0,
+        left: 0,
+        backgroundColor: '#eaeaea',
+        animation: '$entryBlock 2s cubic-bezier(0.87, 0.29, 0.2, 0.97) forwards',
+    },
+    headingText: {
+        opacity: 0,
+        fontFamily: "'Josefin Sans'",
+        fontWeight: 700,
+        animation: '$entryText 1s cubic-bezier(0.87, 0.29, 0.2, 0.97) forwards',
+        animationDelay: '1s',
+    },
+    '@keyframes entryBlock': {
+        '0%': {
+            width: '0%',
+            left: '0',
+        },
+        '50%': {
+            width: '100%',
+            left: '0',
+        },
+        '100%': {
+            width: '0%',
+            left: '100%',
+        },
+    },
+    '@keyframes entryText': {
+        '0%': {
+            opacity: 0,
+        },
+        '100%': {
+            opacity: 1,
+        },
+    },
+    // sign in form container
+    formContainer: {
+        backgroundColor: 'rgb(121 51 51 / 29%)',
+        backdropFilter: 'blur(6px)',
+        maxWidth: '430px',
+        padding: '10px 20px',
+        margin: '0 auto',
     },
     // sign in page discord btn
     discordBtn: {
-        color: "#fff",
-        backgroundImage: "none",
-        backgroundColor: "#5865F2",
-        "&:hover": {
-            backgroundColor: "#313882",
+        color: '#fff',
+        backgroundImage: 'none',
+        backgroundColor: '#5865F2',
+        '&:hover': {
+            backgroundColor: '#313882',
         },
     },
     // loading style
     loading: {
-        display: "grid",
-        placeItems: "center",
-        minHeight: "75vh",
+        display: 'grid',
+        placeItems: 'center',
+        minHeight: '75vh',
     },
 }));
